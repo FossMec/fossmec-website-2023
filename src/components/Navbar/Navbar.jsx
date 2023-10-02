a727e307fe318e394104b8c1a0443773130cd715
Ajay Gonepuri <ajaygonepuri@gmail.com>
2023-10-02 22:06:13 +0530

    navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,40 @@
-
+import React from "react";
 import "./Navbar.css";
-import { useState } from "react";
-import logo from '../../assets/fosspng.svg'
+import logo from "../../assets/fosspng.svg";
 
 const Navbar = () => {
- 
-
   return (
-    <div>
- 
-    </div>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/">
+          <img src={logo} alt="Logo" />
+        </a>
+      </div>
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/events">Events</a>
+          </li>
+          <li>
+            <a href="/team">Team</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/gallery">Gallery</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-button">
+        <a href="/contact">
+          <button>Contact Us</button>
+        </a>
+      </div>
+    </nav>
   );
 };
 
