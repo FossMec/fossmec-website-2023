f253eafc2f6c0c63131de65b070163c30fbfa916
AdithyanA <adithyanayithala@gmail.com>
2023-10-07 00:34:30 +0530

    added events page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
-
+import Events from "./components/Events/Events";
 function App() {
   return (
     <>
@@ -11,6 +11,7 @@ function App() {
         <Navbar/>
         <Hero />
         <About />
+        <Events />
         <Footer />
       </div>
     </>
--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,9 +1,16 @@
-import './Events.css'
-
+import "./Events.css";
+import Boxes from "./Boxes";
 const Events = () => {
   return (
-    <div>Events</div>
-  )
-}
+    <div className=" bg-[#E6F4F4] flex flex-col items-center main-cont">
+      <h1 className="text-[#041B3E] text-[82px] mt-20 font-semibold">EVENTS</h1>
+      <div className="sub-cont flex mt-10 items-center">
+        <Boxes />
+        <Boxes />
+        <Boxes />
+      </div>
+    </div>
+  );
+};
 
-export default Events
+export default Events;
