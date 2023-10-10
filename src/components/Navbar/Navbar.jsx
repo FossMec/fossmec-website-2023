07a57f5dbd74e94425ae22329f50881a01e28739
nikita22panwar <nikita1294@chitkara.edu.in>
2023-10-10 17:26:09 +0530

    change in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 
 import "./Navbar.css";
- import { useState } from "react";
+ import React, { useState } from 'react'
 import logo from '../../assets/fosspng.svg'
 
 const Navbar = () => {
  
 
   return (
+   
     <div className="bg-[#071330] justify-between items-center p-4" style={{
       display:"flex" ,
   }} >
@@ -14,13 +15,13 @@ const Navbar = () => {
  <div className="flex gap-2">
 <a className="text-white" href="#"> Home </a>  
 
-<a className="text-white" href="#"> About </a>  
+<a className="text-white" href="#"> Event </a>  
 
-<a className="text-white" href="#"> Contact </a>  
+<a className="text-white" href="#"> Team</a>  
 
- <a className="text-white" href="#"> Terms of use </a>  
+ <a className="text-white" href="#"> About </a>  
 
-<a className="text-white" href="#"> Join Us </a>  
+<a className="text-white" href="#"> Gallery </a>  
 
 </div>
 <button className="text-white bg-yellow-600 p-2 rounded-full" >Contact Us!</button>
@@ -28,6 +29,9 @@ const Navbar = () => {
     </div>
   );
 };
+
+
+
 
 export default Navbar;
 
