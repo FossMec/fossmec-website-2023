ca83b9447273d46fbb540755cf4ef94759adb3c3
Juanita <114871036+JuanitaCathy@users.noreply.github.com>
2023-10-13 23:01:39 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,45 @@
 import "./About.css";
 
+import React from 'react';
+
 const About = () => {
   return (
-    <div>
-      <div className="grid max-w-screen-xl min-h-[100vh]  px-4  mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <h2 className="About ">About </h2>
-      </div>
-    </div>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap"
+          rel="stylesheet"
+        />
+        <title>Vite + React</title>
+        <style>
+          {`
+            @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Montserrat:wght@200&display=swap');
+            h1 {
+              text-align: center; 
+              color: white; 
+              font-size: 55px;
+              font-family: Inter; 
+              font-weight: 600;
+            }
+            @media all and (max-width: 750px) {
+              h1 {
+                font-size: 42px;
+              }
+            }
+          `}
+        </style>
+      </head>
+      <body style={{ background: '#041424' }}>
+        <h1>ABOUT US</h1>
+        <div id="root"></div>
+        <script type="module" src="/src/components/About/About.jsx"></script>
+      </body>
+    </html>
   );
 };
-
 export default About;
